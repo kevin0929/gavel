42ef4aaf8369a34c6765e3808fcdedffbd27718e
kevin0929 <tn781927@yahoo.com.tw>
2023-11-30 23:53:14 +0800

    [Update] Add new tool - validation program.

--- process.py
+++ process.py
@@ -6,13 +6,15 @@ import numpy as np
 import pandas as pd
 import pywt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler
+
+from utils.preprocess import filter
 
 # filter pandas warning msg
 warnings.filterwarnings("ignore")
 
 
-def process(data_path: str, json_path: str) -> None:
+def process(data_path: str, json_path: str, format: str) -> None:
     """_summary_
 
     Args:
@@ -22,7 +24,6 @@ def process(data_path: str, json_path: str) -> None:
 
     # transform csv file to dataframe and read json file
     df = pd.read_csv(data_path)
-    df.dropna(inplace=True)
 
     with open(json_path, "r") as file:
         time_series_dict = json.load(file)
@@ -46,6 +47,7 @@ def process(data_path: str, json_path: str) -> None:
 
     sensor_list = df["node_address"].unique()
 
+    # to use band pass filter to remove noise
     df = df[
         [
             "node_address",
@@ -59,6 +61,8 @@ def process(data_path: str, json_path: str) -> None:
             "label",
         ]
     ]
+
+    df.dropna(inplace=True)
 
     # config sliding window data parameter
     frequency = 5
@@ -93,8 +97,7 @@ def process(data_path: str, json_path: str) -> None:
             scaler = MinMaxScaler()
             x_data = scaler.fit_transform(x_data)
 
-            encoder = LabelEncoder()
-            y_data = encoder.fit_transform(y_data)
+            y_data = y_data.astype(int)
 
             columns = ["acc_x", "acc_y", "acc_z", "gyro_x", "gyro_y", "gyro_z"]
             x_data = pd.DataFrame(x_data, columns=columns)
@@ -115,36 +118,55 @@ def process(data_path: str, json_path: str) -> None:
     x_data = np.array(data_frame)
     y_data = np.array(label_frame)
 
-    # split dataset into train / test data
-    X_train, X_test, y_train, y_test = train_test_split(
-        x_data, y_data, test_size=0.2, random_state=42, shuffle=True
-    )
+    # if format is train, need to divide dataset into train and test
+    if format == "train":
+        # split dataset into train / test data
+        X_train, X_test, y_train, y_test = train_test_split(
+            x_data, y_data, test_size=0.2, random_state=42, shuffle=True
+        )
 
-    # use cwt to transform sensor data to image-based data
-    x_train = np.ndarray(shape=(X_train.shape[0], 60, 60, 6))
-    scale = range(1, 61)
-    wavelet = "gaus5"
-    for i in range(X_train.shape[0]):
-        for j in range(0, 6):
-            sig = X_train[i, :, j]
-            coeff, freq = pywt.cwt(sig, scale, wavelet)
-            coeff_ = coeff[:, :60]
-            # print(coeff)
-            x_train[i, :, :, j] = coeff_
+        # use cwt to transform sensor data to image-based data
+        x_train = np.ndarray(shape=(X_train.shape[0], 60, 60, 6))
+        scale = range(1, 61)
+        wavelet = "gaus5"
+        for i in range(X_train.shape[0]):
+            for j in range(0, 6):
+                sig = X_train[i, :, j]
+                coeff, freq = pywt.cwt(sig, scale, wavelet)
+                coeff_ = coeff[:, :60]
+                # print(coeff)
+                x_train[i, :, :, j] = coeff_
 
-    x_test = np.ndarray(shape=(X_test.shape[0], 60, 60, 6))
-    scale = range(1, 61)
-    wavelet = "gaus5"
-    for i in range(X_test.shape[0]):
-        for j in range(0, 6):
-            sig = X_test[i, :, j]
-            coeff, freq = pywt.cwt(sig, scale, wavelet)
-            coeff_ = coeff[:, :60]
-            x_test[i, :, :, j] = coeff_
+        x_test = np.ndarray(shape=(X_test.shape[0], 60, 60, 6))
+        scale = range(1, 61)
+        wavelet = "gaus5"
+        for i in range(X_test.shape[0]):
+            for j in range(0, 6):
+                sig = X_test[i, :, j]
+                coeff, freq = pywt.cwt(sig, scale, wavelet)
+                coeff_ = coeff[:, :60]
+                x_test[i, :, :, j] = coeff_
 
-    # save data into numpy type
-    np.savez("train_data.npz", x_train=x_train, y_train=y_train)
-    np.savez("test_data.npz", x_test=x_test, y_test=y_test)
+        # save data into numpy type
+        np.savez("train_data.npz", x_train=x_train, y_train=y_train)
+        np.savez("test_data.npz", x_test=x_test, y_test=y_test)
+
+    elif format == "validation":
+        # split data name into file name
+        filename = data_path.split(".")[0]
+
+        x_valid = np.ndarray(shape=(x_data.shape[0], 60, 60, 6))
+        scale = range(1, 61)
+        wavelet = "gaus5"
+        for i in range(x_data.shape[0]):
+            for j in range(0, 6):
+                sig = x_data[i, :, j]
+                coeff, freq = pywt.cwt(sig, scale, wavelet)
+                coeff_ = coeff[:, :60]
+                x_valid[i, :, :, j] = coeff_
+
+        # save data into numpy type
+        np.savez(f"{filename}.npz", x_valid=x_valid, y_valid=y_data)
 
 
 if __name__ == "__main__":
@@ -152,4 +174,4 @@ if __name__ == "__main__":
     json_path = "train_dataset.json"
 
     # start process data
-    process(data_path, json_path)
+    process(data_path, json_path, "train")
---
+++ validate.py
@@ -0,0 +1,87 @@
+import logging
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.optim import Adam
+from torch.utils.data import DataLoader, TensorDataset
+
+from model import GavelModel
+
+
+def load_data(valid_data_path: str) -> TensorDataset:
+    """_summary_
+
+    Args:
+        train_data_path (str): train data for npz format
+        test_data_path (str): test data for npz format
+
+    Returns:
+        TensorDataset: for GPU training, transform to tensor
+    """
+
+    valid_data = np.load(valid_data_path)
+
+    x_data = valid_data["x_valid"]
+    y_data = valid_data["y_valid"]
+
+    # transform into pytorch tensor data
+    x_data = torch.tensor(x_data).permute(0, 3, 1, 2).float()
+    y_data = torch.tensor(y_data).long()
+
+    # make TensorDataset
+    valid_dataset = TensorDataset(x_data, y_data)
+
+    return valid_dataset
+
+
+def train(
+    model,
+    device,
+    valid_loader,
+) -> None:
+    all_predictions = []
+    all_labels = []
+
+    correct = 0
+    total = 0
+
+    # start validating
+    with torch.no_grad():
+        for inputs, labels in valid_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+            all_predictions.extend(predicted.cpu().numpy())
+            all_labels.extend(labels.cpu().numpy())
+
+    accuracy = 100 * correct / total
+    print(f"Accuracy of the model on the test data: {accuracy:.2f}%")
+
+
+if __name__ == "__main__":
+    # convert data info DataLoader
+    valid_data_path = "test_dataset1_cow157.npz"
+
+    valid_dataset = load_data(valid_data_path)
+    valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=True)
+
+    # build model and set config hyper parameter
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = GavelModel().to(device)
+
+    model_path = "model-ep150-val_loss0.265-val_acc0.896.pth"
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+    # start training
+    train(
+        model=model,
+        device=device,
+        valid_loader=valid_loader,
+    )
